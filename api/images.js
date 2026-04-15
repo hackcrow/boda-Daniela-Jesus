510f510ec3d47134d489ca33604a52a38edf1a17
Rafael Andrade <farar12@gmail.com>
2026-04-15 11:42:03 -0700

    Update images.js

--- api/images.js
+++ api/images.js
@@ -1,1 +1,17 @@
+let images = [];
 
+export default function handler(req, res) {
+  if (req.method === "GET") {
+    return res.status(200).json(images);
+  }
+
+  if (req.method === "POST") {
+    const { url } = req.body;
+
+    if (url && !images.includes(url)) {
+      images.unshift(url);
+    }
+
+    return res.status(200).json({ success: true });
+  }
+}
